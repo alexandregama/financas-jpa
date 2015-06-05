237f22e1bcb7c17f74577809537925fabc2270c7
alexandregama <alexandre.gama.lima@gmail.com>
2015-06-05 13:32:46 -0300

    Adiciona busca pela lista de contas

--- src/main/java/br/com/financas/conta/ContaDao.java
+++ src/main/java/br/com/financas/conta/ContaDao.java
@@ -1,6 +1,10 @@
 package br.com.financas.conta;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 public class ContaDao {
 
@@ -21,5 +25,17 @@ public class ContaDao {
 	public void exclui(Conta conta) {
 		manager.remove(conta);
 	}
+
+	public List<Conta> lista() {
+		String sql = "select c from Conta";
+		TypedQuery<Conta> query = manager.createQuery(sql, Conta.class);
+		return query.getResultList();
+	}
 	
+	@SuppressWarnings("unchecked")
+	public List<Conta> listaComWarning() {
+		String sql = "select c from Conta";
+		Query query = manager.createQuery(sql);
+		return query.getResultList();			
+	}
 }

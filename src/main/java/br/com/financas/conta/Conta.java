6a122343dfa30eb51b6c250dadb77e4d50d7ddc5
alexandregama <alexandre.gama.lima@gmail.com>
2015-06-05 12:24:40 -0300

    Adiciona a Entity Conta

---
+++ src/main/java/br/com/financas/conta/Conta.java
@@ -0,0 +1,43 @@
+package br.com.financas.conta;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "conta")
+@Entity
+public class Conta {
+
+	@Id
+	@GeneratedValue
+	// Mysql - auto increment
+	// SQL Server - identity
+	// Postgres e Oracle - sequence
+	private Long id; //BigInt para o MySQL
+	
+	private String titular;
+	
+	private String banco;
+	
+	private String agencia;
+	
+	private String numero;
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+	
+}

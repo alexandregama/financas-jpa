90bafd030c02306c24188faeeadfb219f3536d99
alexandregama <alexandre.gama.lima@gmail.com>
2015-06-10 22:53:28 -0300

    Adiciona classe para listar as contas via JDBC

---
+++ src/main/java/br/com/financas/conta/ListaContaJdbc.java
@@ -0,0 +1,30 @@
+package br.com.financas.conta;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaContaJdbc {
+
+	public static void main(String[] args) throws SQLException {
+		Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/financas-jpa", "root", "");
+		
+		PreparedStatement stmt = con.prepareStatement("select c.* from Conta c"); //Lembrar que temos 2 tipos de PreparedStatement
+		ResultSet rs = stmt.executeQuery();
+
+		List<Conta> contas = new ArrayList<>();
+		while (rs.next()) {
+			String titular = rs.getString("titular");
+			String banco = rs.getString("banco");
+			String agencia = rs.getString("agencia");
+			String numero = rs.getString("numero");
+			Conta conta = new Conta(titular, banco, agencia, numero);
+			contas.add(conta);
+		}
+		System.out.println(contas);
+	}
+}

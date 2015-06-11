9f09179a18262d83cc5dc42586b7c60fba8e1ce5
alexandregama <alexandre.gama.lima@gmail.com>
2015-06-10 22:47:22 -0300

    Adiciona classe para inserir uma conta via JDBC

---
+++ src/main/java/br/com/financas/conta/InsereContaJdbc.java
@@ -0,0 +1,25 @@
+package br.com.financas.conta;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InsereContaJdbc {
+
+	public static void main(String[] args) throws SQLException {
+		Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/financas-jpa", "root", "");
+		
+		Conta conta = new Conta("Gama", "Itau", "4057", "48723-6");
+		PreparedStatement stmt = con.prepareStatement("insert into Conta (titular, banco, agencia, numero) values (?, ?, ?, ?)");
+		stmt.setString(1, conta.getTitular());
+		stmt.setString(2, conta.getBanco());
+		stmt.setString(3, conta.getAgencia());
+		stmt.setString(4, conta.getNumero());
+		
+		stmt.execute();
+		stmt.close();
+		con.close();
+	}
+	
+}
